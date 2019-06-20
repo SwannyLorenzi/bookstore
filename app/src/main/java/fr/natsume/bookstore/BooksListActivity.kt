fbb961d348790c1e464565120f3a40c7128807a4
Swanny lorenzi <swanny@lorenzi-le.fr>
2019-06-20 22:11:42 +0200

    Initial commit

---
+++ app/src/main/java/fr/natsume/bookstore/BooksListActivity.kt
@@ -0,0 +1,12 @@
+package fr.natsume.bookstore
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BooksListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books_list)
+    }
+}
