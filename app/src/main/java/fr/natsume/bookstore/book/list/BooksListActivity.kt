4f411caa573e0c5ba12d6322e3b0d714afc355f2
Swanny lorenzi <swanny@lorenzi-le.fr>
2019-06-22 13:49:42 +0200

    s63 Bind data <-> acto=ivity

--- app/src/main/java/fr/natsume/bookstore/book/list/BooksListActivity.kt
+++ app/src/main/java/fr/natsume/bookstore/book/list/BooksListActivity.kt
@@ -2,12 +2,44 @@ package fr.natsume.bookstore.book.list
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import fr.natsume.bookstore.R
+import fr.natsume.bookstore.book.Book
+import kotlinx.android.synthetic.main.activity_books_list.*
+import timber.log.Timber
 
-class BooksListActivity : AppCompatActivity() {
+class BooksListActivity : AppCompatActivity(), BooksListAdapter.BooksListAdapterListener {
+
+    private lateinit var viewModel: BooksListViewModel
+    private lateinit var booksAdapter: BooksListAdapter
+    private lateinit var books: MutableList<Book>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books_list)
+
+        books = mutableListOf()
+        booksAdapter = BooksListAdapter(books, this)
+
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@BooksListActivity)
+            adapter = booksAdapter
+        }
+
+        viewModel = ViewModelProviders.of(this).get(BooksListViewModel::class.java)
+        viewModel.books.observe(this, Observer { newBooks -> updateBooks(newBooks!!) })
+    }
+
+    private fun updateBooks(newBooks: List<Book>) {
+        Timber.d("Loading new books: $newBooks")
+        books.clear()
+        books.addAll(newBooks)
+        booksAdapter.notifyDataSetChanged()
+    }
+
+    override fun onBookSelected(book: Book) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
